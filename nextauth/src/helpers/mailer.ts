0cce9206e70c07194f6528f5180fd23463ca405d
Aazim Sadan <aazimsadan99@gmail.com>
2025-05-19 02:52:33 +0530

    learning nodemailer in react

---
+++ nextauth/src/helpers/mailer.ts
@@ -0,0 +1,50 @@
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+    const transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "5dff952fb896fe",
+        pass: "****6a58",
+      },
+    });
+
+    const urlPath = emailType === "VERIFY" ? "verifyemail" : "resetpassword";
+    const subject =
+      emailType === "VERIFY" ? "Verify your email" : "Reset your password";
+    const actionText =
+      emailType === "VERIFY" ? "verify your email" : "reset your password";
+
+    const mailOptions = {
+      from: "aazimsadan99@gmail.com",
+      to: email,
+      subject: subject,
+      html: `<p>Click <a href="${process.env.DOMAIN}/${urlPath}?token=${hashedToken}">here </a> to ${actionText}
+      or copy and paste the link below in your browser.
+      <br> ${process.env.DOMAIN}/${urlPath}?token=${hashedToken}
+      </p>`,
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
